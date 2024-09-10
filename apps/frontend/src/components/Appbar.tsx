4a0a33c5b828b3b44d1fc947618d85c4a2e6261a
Akash-m-SE <akashm9923@gmail.com>
2024-09-10 14:24:13 +0530

    added and updated some components

--- apps/frontend/src/components/Appbar.tsx
+++ apps/frontend/src/components/Appbar.tsx
@@ -39,84 +39,92 @@ const Appbar = () => {
   };
 
   return (
-    <div className="flex border-b justify-between p-4">
-      <div
-        className="flex flex-col justify-center text-2xl font-extrabold"
-        id="navbar-logo"
-      >
-        <Image
-          src="/zapier-logo_black.png"
-          height={100}
-          width={100}
-          alt="logo"
-          priority={true}
-          style={{ width: "auto", height: "auto" }}
-          className="cursor-pointer"
-          onClick={() => {
-            router.push("/");
-          }}
-        />
+    <>
+      <div className="flex border-b justify-between p-4">
+        <div
+          className="flex flex-col justify-center text-2xl font-extrabold"
+          id="navbar-logo"
+        >
+          <Image
+            src="/zapier-logo_black.png"
+            height={100}
+            width={100}
+            alt="logo"
+            priority={true}
+            style={{ width: "auto", height: "auto" }}
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          />
+        </div>
+        <div
+          className="hidden sm:flex md:flex items-center justify-center gap-3"
+          id="navbar-links"
+        >
+          <LinkButton className="w-full h-auto text-center" onClick={() => {}}>
+            Contact Sales
+          </LinkButton>
+
+          <Button
+            className={`${userId ? "block" : "hidden"}`}
+            onClick={() => router.push("/dashboard")}
+          >
+            Dashboard
+          </Button>
+
+          <Button
+            className={`${userId ? "hidden" : "block"}`}
+            onClick={() => {
+              router.push("/login");
+            }}
+          >
+            Login
+          </Button>
+
+          <PrimaryButton
+            className={`${userId ? "hidden" : "block"}`}
+            onClick={() => {
+              router.push("/signup");
+            }}
+          >
+            Signup
+          </PrimaryButton>
+          <Button
+            variant="destructive"
+            className={`${userId ? "block" : "hidden"}`}
+            onClick={() => logoutHandler()}
+          >
+            Logout
+          </Button>
+        </div>
+
+        <div
+          className="flex sm:hidden md:hidden items-center justify-center"
+          id="mobile-navbar"
+        >
+          <Sheet open={isOpen} onOpenChange={setOpen}>
+            <SheetTrigger>
+              <Hamburger toggled={isOpen} toggle={setOpen} />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Menu Bar</SheetTitle>
+                <SheetDescription>Where do you want to go?</SheetDescription>
+                <MobileNavbar router={router} logoutHandler={logoutHandler} />
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
-
       <div
-        className="hidden sm:flex md:flex items-center justify-center gap-3"
-        id="navbar-links"
+        className={`bg-red-500 h-10 w-full text-white font-bold text-base flex items-center justify-center p-6 ${process.env.NODE_ENV === "production" ? "block" : "hidden"}`}
       >
-        <LinkButton className="w-full h-auto text-center" onClick={() => {}}>
-          Contact Sales
-        </LinkButton>
-
-        <Button
-          className={`${userId ? "block" : "hidden"}`}
-          onClick={() => router.push("/dashboard")}
-        >
-          Dashboard
-        </Button>
-
-        <Button
-          className={`${userId ? "hidden" : "block"}`}
-          onClick={() => {
-            router.push("/login");
-          }}
-        >
-          Login
-        </Button>
-
-        <PrimaryButton
-          className={`${userId ? "hidden" : "block"}`}
-          onClick={() => {
-            router.push("/signup");
-          }}
-        >
-          Signup
-        </PrimaryButton>
-        <Button
-          variant="destructive"
-          className={`${userId ? "block" : "hidden"}`}
-          onClick={() => logoutHandler()}
-        >
-          Logout
-        </Button>
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          Server temporarily unavailable. Sorry for the inconvenience!
+        </h4>
       </div>
-
-      <div
-        className="flex sm:hidden md:hidden items-center justify-center"
-        id="mobile-navbar"
-      >
-        <Sheet open={isOpen} onOpenChange={setOpen}>
-          <SheetTrigger>
-            <Hamburger toggled={isOpen} toggle={setOpen} />
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Menu Bar</SheetTitle>
-              <SheetDescription>Where do you want to go?</SheetDescription>
-              <MobileNavbar router={router} logoutHandler={logoutHandler} />
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </div>
+    </>
   );
 };
 export default Appbar;
